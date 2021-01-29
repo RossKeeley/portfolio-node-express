9f024834e42550574f643a077c869a902433add1
Ross Keeley <rjkeeley@gmail.com>
2021-01-29 14:49:24 +0000

    Made static folder, index.html, & changed root route path to index.html

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Ello ello Express');
+  res.sendFile(path.join(__dirname, './static/index.html'));
 });
 
 app.listen(port, (req, res) => {
