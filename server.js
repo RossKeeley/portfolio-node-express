d8f6311d50882629ebf177370e11c60d8e73c230
Ross Keeley <rjkeeley@gmail.com>
2021-01-29 14:55:31 +0000

    Added contact static page and route to it

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './static/index.html'));
 });
 
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/contact.html'));
+});
+
 app.listen(port, (req, res) => {
   console.log(`Express server is listening on port ${port}!`);
 });
